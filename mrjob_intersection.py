d01fdc281843bfe66c25372d8a1d3dd4e890e730
Kyle Magida <kmagida@uchicago.edu>
2015-05-25 15:45:00 -0500

    working confidence calculations

--- mrjob_intersection.py
+++ mrjob_intersection.py
@@ -1,12 +1,21 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 import json
+import csv
 
 BUSINESS_LOC = 0
-DIVIDER = '&&&'
+DIVIDER = ','
+FREQUENCY_FILE = 'frequency/biz_freq.txt'
 
 class IntersectionCount(MRJob):
-  
+  	
+	def configure_options(self):
+		super(IntersectionCount, self).configure_options()
+		self.add_file_option('--freq_table')
+		self.add_passthrough_option('--user_count',type=float,help='Length of user file')
+		self.add_passthrough_option('--review_count',type=float,help='Length of review file')
+
+
 	def mapper_review(self, _, line):
 		line_dict = json.loads(line)
 		user_id = line_dict['user_id']
@@ -27,7 +36,8 @@ class IntersectionCount(MRJob):
 			# Avoid duplicates
 			for y in range(biz_length):
 				if (x != y):
-					yield (businesses[x] + '&&&' + businesses[y]), 1
+					yield (businesses[x] + DIVIDER + businesses[y]), 1
+	
 	def mapper_intersection(self,biz_pair,count):
 		yield biz_pair, count
 
@@ -37,11 +47,51 @@ class IntersectionCount(MRJob):
 	def reducer_intersection(self,biz_pair,count):
 		yield biz_pair, sum(count)
 
+	def final_mapper_init(self):
+		'''
+		file goes here
+		'''
+		f = open(self.options.freq_table,'rU')
+		self.dictionary = {}
+		for line in f:
+			items = line.split()
+			self.dictionary[items[0].strip('"')] = items[1]
+		self.users = self.options.user_count
+		self.reviews = self.options.review_count
+		
+
+
+	def final_mapper(self,biz_pair,intersection_count):
+
+		businesses = biz_pair.split(DIVIDER)
+		count_a = float(self.dictionary[businesses[0]])
+		count_b = float(self.dictionary[businesses[1]])
+		prob_a = count_a / self.users
+		prob_b = count_b / self.reviews
+		probability_a_b = intersection_count / self.users
+		confidence = probability_a_b / prob_a
+		lift = confidence / prob_b
+		yield biz_pair, [prob_a,prob_b,confidence, lift]
+
+
 	def steps(self):
 		return [
 			MRStep(mapper=self.mapper_review,combiner=self.combiner_review,reducer=self.reducer_review),
-			MRStep(mapper=self.mapper_intersection,combiner=self.combiner_intersection,reducer=self.reducer_intersection)
+			MRStep(mapper=self.mapper_intersection,combiner=self.combiner_intersection,reducer=self.reducer_intersection),
+			MRStep(mapper_init=self.final_mapper_init,mapper=self.final_mapper)
 			]
+
+# def make_dictionary(table):
+	
+# 	rv = {}
+# 	for line in table:
+# 		print line
+# 		if line[0] not in rv:
+# 			rv[line[0]] = line[1]
+
+# 	return rv
+
+
 if __name__ == '__main__':
 	IntersectionCount.run()
 
--- run_intersection.py
+++ run_intersection.py
@@ -5,15 +5,23 @@ OUTPUT_FILE = 'MRreview_Intersection_count.csv'
 
 if __name__ == '__main__':
     # Creates an instance of our MRJob subclass
+    
+
     job = IntersectionCount(args=sys.argv[1:])
     with job.make_runner() as runner:
         # Run the job
         runner.run()
         filename = open(OUTPUT_FILE,'w')
-        filename.write('biz_1&&&biz_2, total_count\n')
+        filename.write('biz_1, biz_2, prob_a, prob_b, confidence, lift\n')
 
 
         # Process the output
         for line in runner.stream_output():
             key, value = job.parse_output_line(line)
-            filename.write(str(key) + ',' + str(value) + '\n')
+            filename.write(str(key) + ',' + ",".join([str(x) for x in value]) + '\n')
+
+def line_count(filename):
+    f = open(filename)
+    for i, l in enumerate(f):
+        pass
+    return i + 1
